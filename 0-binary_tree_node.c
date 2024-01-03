c45b1a471adb3216391d1c56ce0639825dce5b22
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-03 06:35:38 -0800

    Fixing bugs

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tempNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *tempNode;
+
+	tempNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (!tempNode)
 		return (NULL);
