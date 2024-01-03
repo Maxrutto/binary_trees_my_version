6e6aa0688da58cac6b31381fb10956f64ba9d9d5
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-03 06:38:31 -0800

    More changes

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,10 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,3 +27,9 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
+
+#endif
